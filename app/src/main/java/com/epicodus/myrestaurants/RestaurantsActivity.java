87d9c3c04fe23765d213e00c7b6532e839ff45fc
shy <shyamal.punekar@gmail.com>
2017-10-10 14:28:45 -0700

    Add RestaurantsActivityInstrumentationTest

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -11,29 +11,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class RestaurantsActivity extends AppCompatActivity {
-
     private TextView mLocationTextView;
-
     private ListView mListView;
-
-    //add restaurants name
-    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
-            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
-            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
-            "Lardo", "Portland City Grill", "Fat Head's Brewery",
-            "Chipotle", "Subway"};
-
+    private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food",
+            "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar",
+            "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
 
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
 
-        mListView = (ListView) findViewById(R.id.listView);
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
         mListView.setAdapter(adapter);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -46,7 +38,61 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-
         mLocationTextView.setText("Here are all the restaurants near: " + location);
     }
 }
+
+
+
+// /package com.epicodus.myrestaurants;
+//
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//public class RestaurantsActivity extends AppCompatActivity {
+//
+//    private TextView mLocationTextView;
+//
+//    private ListView mListView;
+//
+//    //add restaurants name
+//    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+//            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+//            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+//            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+//            "Chipotle", "Subway"};
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_restaurants);
+//
+//        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+//
+//        mListView = (ListView) findViewById(R.id.listView);
+//
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+//        mListView.setAdapter(adapter);
+//
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String restaurant = ((TextView)view).getText().toString();
+//                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        Intent intent = getIntent();
+//        String location = intent.getStringExtra("location");
+//
+//        mLocationTextView.setText("Here are all the restaurants near: " + location);
+//    }
+//}
--- app/src/test/java/com/epicodus/myrestaurants/RestaurantsActivityTest.java
+++ app/src/test/java/com/epicodus/myrestaurants/RestaurantsActivityTest.java
@@ -4,10 +4,14 @@ import android.os.Build;
 import android.widget.ListView;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 @RunWith(RobolectricGradleTestRunner.class)
@@ -24,4 +28,11 @@ public class RestaurantsActivityTest {
         activity = Robolectric.setupActivity(RestaurantsActivity.class);
         mRestaurantListView = (ListView) activity.findViewById(R.id.listView);
     }
+
+    @Test
+    public void restaurantListViewPopulates() {
+        assertNotNull(mRestaurantListView.getAdapter());
+        assertEquals(mRestaurantListView.getAdapter().getCount(), 16);
+    }
+
 }
