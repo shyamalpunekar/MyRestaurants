a5871bd42fad81f015f4e47d14ace19a1ca8415a
shy <shyamal.punekar@gmail.com>
2017-10-15 18:18:27 -0700

    Create new request with OkHttp

--- app/src/main/java/com/epicodus/myrestaurants/YelpService.java
+++ app/src/main/java/com/epicodus/myrestaurants/YelpService.java
@@ -1,7 +1,9 @@
 package com.epicodus.myrestaurants;
 
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 
 public class YelpService {
@@ -9,6 +11,17 @@ public class YelpService {
     public static void findRestaurants(String location,  Callback callback) {
 
         OkHttpClient client = new OkHttpClient.Builder().build();
+
+
+        HttpUrl.Builder urlBuilder= HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
+
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", Constants.YELP_TOKEN)
+                .build();
 
 
     }
