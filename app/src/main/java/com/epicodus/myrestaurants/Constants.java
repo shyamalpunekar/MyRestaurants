8001ecd6511a34683eeb7a03f1ddb6428aed592b
shy <shyamal.punekar@gmail.com>
2017-10-15 18:01:20 -0700

    Add OkHttpClient send request

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -4,4 +4,6 @@ package com.epicodus.myrestaurants;
 public class Constants {
 
     public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=restaurants";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
 }
---
+++ app/src/main/java/com/epicodus/myrestaurants/test.java
@@ -0,0 +1,8 @@
+package com.epicodus.myrestaurants;
+
+/**
+ * Created by spuek on 10/15/17.
+ */
+
+public class test {
+}
