81e3246edb9a99c80cbf1645da21223c0a3e344b
shy <shyamal.punekar@gmail.com>
2017-10-27 15:18:23 -0700

    Add onCreateoptionMenu

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -1,25 +1,16 @@
 package com.epicodus.myrestaurants.ui;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -86,6 +77,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     startActivity(intent);
                 }
             }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
 
 //            public void saveLocationToFirebase(String location){
